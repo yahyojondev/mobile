d16c2949c2103a1d2230155ab6c6c881169bddc4
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-20 23:47:43 +0500

    work

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let a = prompt(" So'zni kiritng:");
+let a = prompt(
+  " So'zni kiritng   1.mashina,2.opa,3.kuchuk,4.tog'a,5.dada,6.ona,7.suv,8.aka,9.hayrli kun:"
+);
 if (a === "salom") {
   alert("hello");
 } else if (a === "hayrli kun") {
