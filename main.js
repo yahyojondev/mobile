27e28717e1e2749b7564eb2ac333ea85622f2b84
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-20 23:40:42 +0500

    hello

---
+++ main.js
@@ -0,0 +1,22 @@
+let a = prompt(" So'zni kiritng:");
+if (a === "salom") {
+  alert("hello");
+} else if (a === "hayrli kun") {
+  alert("Good day");
+} else if (a === "mashina") {
+  alert("car");
+} else if (a === "kuchuk") {
+  alert("dog");
+} else if (a === "tog'a") {
+  alert("uncle");
+} else if (a === "dada") {
+  alert("father");
+} else if (a === "ona") {
+  alert("mother");
+} else if (a === "aka") {
+  alert("brother");
+} else if (a === "opa") {
+  alert("sister");
+} else if (a === "suv") {
+  alert("watter");
+}
